ca905d31a70c89c8dac0047a805d13fcdd4a2bdc
ajmods <dandadrew02@gmail.com>
2026-03-06 15:43:40 +0900

    hide watermark

--- levelGenWeb.py
+++ levelGenWeb.py
@@ -9,6 +9,17 @@ from fontTools.tfmLib import MATHSY
 
 from levelGen import find_paths
 
+# code to hide the watermark using CSS
+
+# #MainMenu to hide the burger menu at the top-right side
+# footer to hide the ```made with streamlit``` mark
+hide = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide, unsafe_allow_html=True)
 
 # --- KEEP YOUR find_paths FUNCTION HERE ---
 
